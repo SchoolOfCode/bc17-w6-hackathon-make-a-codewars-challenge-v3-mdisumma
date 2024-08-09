a0f65f2bb5f53a1b0433df60897ca33aefb5be19
Marco Di Summa <mdisumma@yahoo.com>
2024-08-09 10:35:11 +0100

    solution branch

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-export function buildPyramid(rows, pyramidChar, spaceChar) {}
+export function buildPyramid(rows, pyramidChar, spaceChar) {
+  let string = "";
+  for (let rowCounter = 1; rowCounter <= rows; rowCounter++) {
+    if (rowCounter % 2 === 1) {
+      string +=
+        spaceChar.repeat((rows - rowCounter) / 2) +
+        pyramidChar.repeat(rowCounter) +
+        spaceChar.repeat((rows - rowCounter) / 2) +
+        "\n";
+    }
+  }
+  return string;
+}
